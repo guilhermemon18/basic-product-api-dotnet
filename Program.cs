7b161f7ba568724584b8d66afcc0e05ec56886d9
guilhermemon18 <guilhermemonsani7@gmail.com>
2024-10-19 14:39:39 -0300

    first commit: projeto concluído

---
+++ Program.cs
@@ -0,0 +1,80 @@
+using System.Security.Permissions;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapPost("/", () => new { name = "Stephany Batista", age = 35 });
+app.MapGet("/AddHeader", (HttpResponse response) =>{
+    response.Headers.Add("Teste", "Stephany Batista");
+    return new { name = "Stephany Batista", age = 35 };
+});
+
+app.MapPost("/saveproduct", (Product product) =>{
+    ProductRepository.Add(product);
+});
+
+//api.app.com/user/{code}
+app.MapGet("/getproduct/{code}", ([FromRoute] string code) =>{
+    var product = ProductRepository.GetProductByCode(code);
+    return product;
+});
+
+app.MapPut("/editproduct", (Product product) =>{
+    var productSaved = ProductRepository.GetProductByCode(product.Code);
+    productSaved.Name = product.Name;
+});
+
+app.MapDelete("/deleteproduct/{code}", ([FromRoute] string code) =>{
+    var productSaved = ProductRepository.GetProductByCode(code);
+    ProductRepository.Remove(productSaved);
+
+});
+
+// //api.app.com/users?datastart={date}&dataend={date}
+// app.MapGet("/getproduct", ([FromQuery] string dateStart, [FromQuery] string dataEnd) => {
+//     return dateStart + " - " + dataEnd;
+// });
+
+
+
+app.Run();
+
+
+public static class ProductRepository{
+    public static List<Product> Products { get; set; }
+
+
+    // public ProductRepository(){
+    //     Products = new List<Product>();
+    // }
+
+    public static void Add(Product product)    {
+        if (Products == null)
+        {
+            Products = new List<Product>();
+        }
+        Products.Add(product);
+    }
+
+    public static Product GetProductByCode(string code)    {
+        return Products.FirstOrDefault(p => p.Code == code);
+    }
+
+    public static void Remove(Product product)    {
+        Products.Remove(product);
+    }
+
+}
+
+public class Product{
+    public string Code { get; set; }
+    public string Name { get; set; }
+
+    // Construtor com parâmetros
+    public Product(string code, string name)    {
+        Code = code;
+        Name = name;
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# API PRODUTOS
+
+**Introdução**
+
+Esta API fornece funcionalidades para gerenciar produtos em um sistema. Ela é construída com ASP.NET Core e utiliza a própria memória RAM como um banco de dados.
+
+**Como usar a API**
+
+Para utilizar esta API, você precisará de um cliente HTTP como Postman ou Insomnia para fazer as requisições.
+
+**Endpoints**
+
+| Método | URL | Parâmetros | Resposta | Descrição |
+|---|---|---|---|---|
+| POST | /saveproduct | { "code": "1", "name": "HDD" } | - | Cria um novo produto |
+| GET | /getproduct/{code}| id (int) | JSON (product) | Retorna um produto específico |
+| PUT | /editproduct | { "code": "1", "name": "Memória RAM" } | -| Atualiza um produto existente |
+| DELETE | /deleteproduct/{code} | id (int) | - | Deleta um usuário |
